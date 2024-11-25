461a95cdd72c80e4be4d82fb49f364ca95d1f630
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2024-11-25 16:50:22 +0500

    25.11.2024 12343452345123

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -148,10 +148,67 @@ function RootLayoutNav() {
         return;
       }
       const expoPushToken = await SecureStore.getItemAsync(EXPO_PUSH_TOKEN_KEY)
-      if (expoPushToken) {
-        console.log(expoPushToken);
-        
-        if (user) {
+      if (!expoPushToken) {
+        try {
+          const tokenData = await Notifications.getExpoPushTokenAsync({projectId: "44ab56bf-15dd-4f12-9c01-c29f592dc6c9"});
+          const token = tokenData.data;
+          await SecureStore.setItemAsync(EXPO_PUSH_TOKEN_KEY, token);
+        } catch (error) {
+          await useHttp
+            .post<any>("/expoTokenCheck", { where: "getExpoPushTokenAsync error", error })
+            .then((res: any) => {
+              console.log("expoToken check");
+            })
+            .catch((err: any) => {
+              console.log("Ошибка при отправке expoToken:", err);
+            });
+        }
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (user?.mail) {
+        const expoPushToken = await SecureStore.getItemAsync(EXPO_PUSH_TOKEN_KEY)
+        if (!expoPushToken) {
+          try {
+            const tokenData = await Notifications.getExpoPushTokenAsync({projectId: "44ab56bf-15dd-4f12-9c01-c29f592dc6c9"});
+            const token = tokenData.data;
+            await useHttp
+            .post("/updateClientDataMobile", {
+              mail: user.mail,
+              field: "expoPushToken",
+              value: expoPushToken,
+            })
+            .then(() => {
+              dispatch(
+                setUser({
+                  ...user,
+                  expoPushToken: token,
+                })
+              );
+            })
+            .catch((err: { response: { data: { message: any; }; }; }) => {
+              dispatch(
+                setError({
+                  error: true,
+                  errorMessage: err?.response?.data?.message,
+                })
+              );
+            });
+            await SecureStore.setItemAsync(EXPO_PUSH_TOKEN_KEY, token);
+          } catch (error) {
+            await useHttp
+              .post<any>("/expoTokenCheck", { where: "getExpoPushTokenAsync error", error })
+              .then((res: any) => {
+                console.log("expoToken check");
+              })
+              .catch((err: any) => {
+                console.log("Ошибка при отправке expoToken:", err);
+              });
+          }
+        } else {
           await useHttp
             .post("/updateClientDataMobile", {
               mail: user.mail,
@@ -173,55 +230,12 @@ function RootLayoutNav() {
                   errorMessage: err?.response?.data?.message,
                 })
               );
-          });
-        }
-      }
-      if (!expoPushToken) {
-        try {
-          const tokenData = await Notifications.getExpoPushTokenAsync({projectId: "44ab56bf-15dd-4f12-9c01-c29f592dc6c9"});
-          const token = tokenData.data;
-          console.log("token: ", token);
-          if (user) {
-            await useHttp
-              .post("/updateClientDataMobile", {
-                mail: user.mail,
-                field: "expoPushToken",
-                value: token,
-              })
-              .then(() => {
-                console.log("updateClientDataMobile in layout res");
-                
-                dispatch(
-                  setUser({
-                    ...user,
-                    expoPushToken: token,
-                  })
-                );
-              })
-              .catch((err: { response: { data: { message: any; }; }; }) => {
-                console.log("updateClientDataMobile in layout error");
-                dispatch(
-                  setError({
-                    error: true,
-                    errorMessage: err?.response?.data?.message,
-                  })
-                );
-              });
-          }
-          await SecureStore.setItemAsync(EXPO_PUSH_TOKEN_KEY, token);
-        } catch (error) {
-          await useHttp
-            .post<any>("/expoTokenCheck", { where: "getExpoPushTokenAsync error", error })
-            .then((res: any) => {
-              console.log("expoToken check");
-            })
-            .catch((err: any) => {
-              console.log("Ошибка при отправке expoToken:", err);
             });
         }
       }
-    })();
-  }, []);
+    })
+    
+  }, [user])
 
   return (
     <View
